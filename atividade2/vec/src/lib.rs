f1f17f8a6e6224a5b55763b2a8e6b86638990216
Leonardo Valerio Morales <leovmorales@hotmail.com>
2023-10-16 15:04:42 -0300

    Vec3 Tests

--- atividade2/vec/src/lib.rs
+++ atividade2/vec/src/lib.rs
@@ -42,23 +42,23 @@ pub fn v3_to_string(u: &Vec3) -> String {
     u.values.iter().map(|c| c.to_string()).collect::<Vec<String>>().join(" ")
 }
 
-impl ops::Add<Vec3> for &Vec3 {
+impl ops::Add<&Vec3> for &Vec3 {
     type Output = Vec3;
-    fn add(self, rhs: Vec3) -> Self::Output {
+    fn add(self, rhs: &Vec3) -> Self::Output {
         Vec3 { values: vec![self.values[0]+rhs.values[0],self.values[1]+rhs.values[1],self.values[2]+rhs.values[2]] }
     }
 }
 
-impl ops::Sub<Vec3> for &Vec3 {
+impl ops::Sub<&Vec3> for &Vec3 {
     type Output = Vec3;
-    fn sub(self, rhs: Vec3) -> Self::Output {
+    fn sub(self, rhs: &Vec3) -> Self::Output {
         Vec3 { values: vec![self.values[0]-rhs.values[0],self.values[1]-rhs.values[1],self.values[2]-rhs.values[2]] }
     }
 }
 
-impl ops::Mul<Vec3> for &Vec3 {
+impl ops::Mul<&Vec3> for &Vec3 {
     type Output = Vec3;
-    fn mul(self, rhs: Vec3) -> Self::Output {
+    fn mul(self, rhs: &Vec3) -> Self::Output {
         Vec3 { values: vec![self.values[0]*rhs.values[0],self.values[1]*rhs.values[1],self.values[2]*rhs.values[2]] }
     }
 }
@@ -106,18 +106,114 @@ pub fn write_color(pixel_color: Vec3) {
 
 /// TESTS
 
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn sum_test() {
+        let x: Vec3 = Vec3::new(2.0, 3.0, 4.0);
+        let other: Vec3 = Vec3::new(2.0, 3.0, 4.0);
+        let result = x.sum(&other);
+        assert_eq!(result.values[0], 4.0);
+        assert_eq!(result.values[1], 6.0);
+        assert_eq!(result.values[2], 8.0);
+    }
+
+    #[test]
+    fn multiply_test() {
+        let x: Vec3 = Vec3::new(2.0, 3.0, 4.0);
+        let other = 3.0;
+        let result = x.multiply(other);
+        assert_eq!(result.values[0], 6.0);
+        assert_eq!(result.values[1], 9.0);
+        assert_eq!(result.values[2], 12.0);
+    }
+
+    #[test]
+    fn div_test() {
+        let x: Vec3 = Vec3::new(2.0, 3.0, 4.0);
+        let other = 2.0;
+        let result = x.divide(other);
+        assert_eq!(result.values[0], 1.0);
+        assert_eq!(result.values[1], 1.5);
+        assert_eq!(result.values[2], 2.0);
+    }
+
+    #[test]
+    fn length_test() {
+        let x = Vec3::new(2.0,3.0,4.0);
+        assert_eq!(5.38516480713450403125071049154032955629512016164478883768038867,x.length())
+    }
+
+    #[test]
+    fn inline_sum_test() {
+        let x = Vec3::new(2.0,3.0,4.0);
+        let other: Vec3 = Vec3::new(3.0,5.0,8.0);
+        assert_eq!(5.0,(&x+&other).values[0]);
+        assert_eq!(8.0,(&x+&other).values[1]);
+        assert_eq!(12.0,(&x+&other).values[2]);
+    }
+
+    #[test]
+    fn inline_sub_test() {
+        let x = Vec3::new(2.0,3.0,4.0);
+        let other: Vec3 = Vec3::new(3.0,5.0,8.0);
+        assert_eq!(-1.0,(&x-&other).values[0]);
+        assert_eq!(-2.0,(&x-&other).values[1]);
+        assert_eq!(-4.0,(&x-&other).values[2]);
+    }
+    #[test]
+    fn inline_mul_vec_test() {
+        let x = Vec3::new(2.0,3.0,4.0);
+        let other: Vec3 = Vec3::new(3.0,5.0,8.0);
+        assert_eq!(6.0,(&x*&other).values[0]);
+        assert_eq!(15.0,(&x*&other).values[1]);
+        assert_eq!(32.0,(&x*&other).values[2]);
+    }
+
+    #[test]
+    fn inline_mul_factor_test() {
+        let x = 10.0;
+        let other: Vec3 = Vec3::new(3.0,5.0,8.0);
+        assert_eq!(30.0,(x*&other).values[0]);
+        assert_eq!(50.0,(x*&other).values[1]);
+        assert_eq!(80.0,(x*&other).values[2]);
+    }
+
+    #[test]
+    fn inline_mul_factor_inverted_test() {
+        let x = 10.0;
+        let other: Vec3 = Vec3::new(3.0,5.0,8.0);
+        assert_eq!(30.0,(&other*x).values[0]);
+        assert_eq!(50.0,(&other*x).values[1]);
+        assert_eq!(80.0,(&other*x).values[2]);
+    }
+
+    #[test]
+    fn inline_div_factor_test() {
+        let x = Vec3::new(23.4,85.2,90.1);
+        let other: f64 = 2.5;
+        assert_eq!(9.36,(&x/other).values[0]);
+        assert_eq!(34.080000000000005,(&x/other).values[1]);
+        assert_eq!(36.04,(&x/other).values[2]);
+    }
+
+    #[test]
+    fn dot_test() {
+        let x = Vec3::new(23.4,85.2,90.1);
+        let other: Vec3 = Vec3::new(3.3, 2.1, 3.9);
+        let result = dot(&x,&other);
+        assert_eq!(607.53,result);
+    }
+
+    #[test]
+    fn cross_test() {
+        let x = Vec3::new(23.4,85.2,90.1);
+        let other: Vec3 = Vec3::new(3.3, 2.1, 3.9);
+        let result = cross(&x,&other);
+        assert_eq!(143.07000000000002,result.values[0]);
+        assert_eq!(206.07,result.values[1]);
+        assert_eq!(-232.01999999999998,result.values[2]);
     }
 }
